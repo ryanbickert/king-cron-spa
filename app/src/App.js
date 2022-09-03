c59727c27763c7da49a2c6ab37c1ac74bc547ff8
Ryan Bickert <rnbickert@gmail.com>
2022-09-03 14:07:26 -0600

    Map Zoom-Pan-Pinch Work
    Started implementing the interactable map.

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 import CustomButtons from './components/CustomButtons';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import { positions } from '@mui/system';
+import woodPanelLg from './images/wood-panel-lg.png';
+import cronMap from './images/cron-map.png';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 function App() {
+  const [scale, setScale] = useState(6);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <CustomButtons/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="WoodPanel">
+        <Grid container>
+          <Grid item xs={12}>
+            <TransformWrapper
+              centerOnInit
+              initialScale={scale}
+              initialPositionX={-3000}
+              initialPositionY={-1620}
+              panning={{ disabled: scale >= 1, velocityDisabled: true }}
+              alignmentAnimation={{ sizeX: 20, sizeY: 20, velocityAlignmentTime: 50}}
+              onZoom={(ref) => {
+                setScale(ref.state.scale);
+                console.log(scale);;
+              }}
+            >
+              <TransformComponent>
+                <img
+                  src={cronMap}
+                  alt="Map"
+                  style={{
+                    width: '100vw',
+                    height: '100vh'
+                  }} />
+              </TransformComponent>
+            </TransformWrapper>
+          </Grid>
+        </Grid>
+      </div >
+      <div className="Stone">
+        hi
+      </div >
     </div>
   );
 }
--- functions/cron-functions/index.js
+++ functions/cron-functions/index.js
@@ -1,18 +1,6 @@
-console.log("running");
-
 const express = require('express');
 const app = express();
 
-function textlog()
-{
-	console.log("pls")
-}
-
-textlog();
-
 app.post('/hi', express.json(), async (req, res) => {
-	//const playerName = req.body.playerName;
-	//const score = req.body.score;
 	console.log("hello");
-	res.status(200);
 });
